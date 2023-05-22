e4b359586f7c1aaa9a92fb710182b3bc963d2c26
Tribeoftech <gratefulgrowing333@gmail.com>
2023-05-22 15:42:18 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""Task 0 - Project 0x05:: Add Integer"""
+"""0-add_integer.py and tests/0-add_integer.txt"""
 
 
 def add_integer(a, b=98):
-    """Adds two ints together"""
-    a = (round(a) if isinstance(a, (float, int)) else None)
-    b = (round(b) if isinstance(b, (float, int)) else None)
-    if a is None:
-        raise TypeError("a must be an integer")
-    if b is None:
-        raise TypeError("b must be an integer")
-    return int(round(a + b))
+    """
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
+    """
+    if not isinstance(a, int):
+        if isinstance(a, float):
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if isinstance(b, float):
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
