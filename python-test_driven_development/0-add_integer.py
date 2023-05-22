9d3f70d711e359888b4ab1657aaeb8eb111b9ddb
Tribeoftech <gratefulgrowing333@gmail.com>
2023-05-22 15:12:07 -0500

    t push

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-
-"""
-add_integer - adds 2 integers
-a - first integer for addition
-b - second integer for addition
-Return: sum of addittion
-"""
+"""Task 0 - Project 0x05:: Add Integer"""
 
 
 def add_integer(a, b=98):
-    """This def adds two integers and returns the sum.
-    Float arguments are typecasted to ints before additon is performed.
-    Raises a TypeError if either a or b is a non-integer or non-float."""
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    """Adds two ints together"""
+    a = (round(a) if isinstance(a, (float, int)) else None)
+    b = (round(b) if isinstance(b, (float, int)) else None)
+    if a is None:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if b is None:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))6-max_integer.py
+    return int(round(a + b))
