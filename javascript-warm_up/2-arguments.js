6ccaad8ccb81c891e8146cfaa28912f981d0d7b8
Tribeoftech <gratefulgrowing333@gmail.com>
2023-07-28 12:49:55 -0500

    2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,19 @@
+// 2-arguments.js
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+
+if (argsCount >= 2) {
+  console.log(`Second argument: ${process.argv[2]}`);
+}
+
+if (argsCount >= 3) {
+  console.log(`Third argument: ${process.argv[3]}`);
+}
+
