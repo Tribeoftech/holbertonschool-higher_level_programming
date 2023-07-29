e09765840bfb372161be3286211ce2e93d21d67b
Tribeoftech <gratefulgrowing333@gmail.com>
2023-07-28 20:22:51 -0500

    README.md

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,35 +1,12 @@
-<<<<<<< HEAD
-#!/usr/bin/node
-=======
-<<<<<<< HEAD
-#!/usr/bin/node
-const Args = process.argv.slice(2);
-if (Args.length === 0) {
-  console.log('No argument');
-} else if (Args.length === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
-=======
-// 2-arguments.js
->>>>>>> refs/remotes/origin/master
-const argsCount = process.argv.length - 2;
+#!/usr/bin/env node
 
-if (argsCount === 0) {
+const argsLength = process.argv.length;
+
+if (argsLength === 2) {
   console.log("No argument");
-} else if (argsCount === 1) {
+} else if (argsLength === 3) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
 }
 
-if (argsCount >= 2) {
-  console.log(`Second argument: ${process.argv[2]}`);
-}
-
-if (argsCount >= 3) {
-  console.log(`Third argument: ${process.argv[3]}`);
-}
-
->>>>>>> refs/remotes/origin/master
