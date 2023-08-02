4d9fdc1f293bf13790d890c67950227c70da15c7
Alex Justin Dipboye <113186733+Tribeoftech@users.noreply.github.com>
2023-08-02 10:34:31 -0500

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const argsLength = process.argv.length;
 
-if (argsLength !== 3) {
+if (argsLength !== 1) {
   console.log('No argument');
-} else if (argsLength === 3) {
+} else if (argsLength === 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
